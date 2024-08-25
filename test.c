3de6c0f66bf703533be370fbbad9ab8bee7d5234
Pratham <prathams1310@gmail.com>
2024-08-25 23:37:01 +0530

    testing formattings and extra features

--- test.c
+++ test.c
@@ -79,6 +79,50 @@ void test_empty_subsubsection() {
     free(result);
 }
 
+/**
+ * @brief Test conversion of bold text.
+ */
+void test_bold() {
+    struct treeNode *node1 = create_node_with_type(PLAIN_TEXT,"text","this is a text",NULL,NULL);
+    struct treeNode *node = create_node_with_type(BOLD, "bold", "", node1, NULL);
+    char *result = markdownCode(node);
+    CU_ASSERT_STRING_EQUAL_FATAL(result, "**this is a text**");
+    free(result);
+}
+
+/**
+ * @brief Test conversion of italic text.
+ */
+
+void test_italic() {
+    struct treeNode *node1 = create_node_with_type(PLAIN_TEXT,"text","this is a text",NULL,NULL);
+    struct treeNode *node = create_node_with_type(ITALIC, "italic", "", node1, NULL);
+    char *result = markdownCode(node);
+    CU_ASSERT_STRING_EQUAL_FATAL(result, "*this is a text*");
+    free(result);
+}
+
+/**
+ * @brief Test conversion of bold and italic text combined.
+ */
+
+void test_bold_with_italic() {
+    struct treeNode *node1 = create_node_with_type(PLAIN_TEXT,"text","this is a text",NULL,NULL);
+    struct treeNode *boldNode = create_node_with_type(BOLD, "bold", "", node1, NULL);
+    struct treeNode *italicNode = create_node_with_type(ITALIC, "italic", "", boldNode, NULL);
+    char *result = markdownCode(italicNode);
+    CU_ASSERT_STRING_EQUAL_FATAL(result, "***this is a text***");
+    free(result);
+}
+
+void test_monospace() {
+    struct treeNode *node1 = create_node_with_type(PLAIN_TEXT, "text", "monospace text", NULL, NULL);
+    struct treeNode *node = create_node_with_type(MONO, "mono", "", node1, NULL);
+    char *result = markdownCode(node);
+    CU_ASSERT_STRING_EQUAL_FATAL(result, "`monospace text`");
+    free(result);
+}
+
 
 int main() {
     CU_initialize_registry();
@@ -92,7 +136,11 @@ int main() {
     CU_add_test(suite, "test_empty_subsection", test_empty_subsection);
     CU_add_test(suite, "test_subsubsection", test_subsubsection);
     CU_add_test(suite, "test_empty_subsubsection", test_empty_subsubsection);
-    
+    CU_add_test(suite, "test_bold", test_bold);
+    CU_add_test(suite, "test_italic", test_italic);
+    CU_add_test(suite, "test_bold_with_italic", test_bold_with_italic);
+    CU_add_test(suite, "test_monospace", test_monospace);
+
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
